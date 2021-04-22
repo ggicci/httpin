ecd755d13674e9d3863c751ad71a96f83cdf05f1
Ggicci <ggicci.t@gmail.com>
2021-04-22 10:06:04 +0800

    refactor: test cases

--- httpin_test.go
+++ httpin_test.go
@@ -4,37 +4,12 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
-	"io"
 	"net/url"
 	"testing"
 	"time"
 
 	"github.com/ggicci/httpin"
 )
-
-type TestCase struct {
-	Title         string
-	InputType     interface{}
-	InputForm     url.Values
-	InputBody     io.Reader
-	Expected      interface{}
-	ExpectedError error
-}
-
-func (c *TestCase) Check(t *testing.T, got interface{}, err error) {
-	if !errors.Is(err, c.ExpectedError) {
-		t.Errorf("parse failed, expect error %v, got %v", c.ExpectedError, err)
-		return
-	}
-
-	// Compare got vs. expected.
-	left, _ := json.Marshal(c.Expected)
-	right, _ := json.Marshal(got)
-	if bytes.Compare(left, right) != 0 {
-		t.Errorf("parse failed, expected %s, got %s", left, right)
-		return
-	}
-}
 
 type Pagination struct {
 	Page    int `query:"page"`
@@ -67,7 +42,29 @@ type MessageQuery struct {
 	Cursor
 }
 
-func parseFormAndCheck(t *testing.T, inputStruct,expected, got interface{} ) {
+func check(t *testing.T, expected, got interface{}) {
+	// Expecting error.
+	expectedError, isExpectingError := expected.(error)
+	if isExpectingError {
+		if gotError, ok := got.(error); ok {
+			if errors.Is(gotError, expectedError) {
+				return
+			}
+		}
+		t.Errorf("parse failed, expected error %v, got %v", expectedError, got)
+		return
+	}
+
+	// Expecting struct output.
+	left, _ := json.Marshal(expected)
+	right, _ := json.Marshal(got)
+	if bytes.Compare(left, right) != 0 {
+		t.Errorf("parse failed, expected %s, got %s", left, right)
+		return
+	}
+}
+
+func parseFormAndCheck(t *testing.T, form url.Values, inputStruct, expected interface{}) {
 	engine, err := httpin.NewEngine(inputStruct)
 	if err != nil {
 		t.Errorf("unable to create engine: %s", err)
@@ -75,68 +72,50 @@ func parseFormAndCheck(t *testing.T, inputStruct,expected, got interface{} ) {
 		return
 	}
 
-	if expectedError, ok := expected.(error); ok {
-		if !errors.Is(err, expectedError) {
-			t.Errorf("parse failed, expect error %v, got %v", expectedError, err)
-			return
-		}
+	got, err := engine.ReadForm(form)
+	if err != nil {
+		check(t, expected, err)
 	} else {
-		// Compare got vs. expected.
-		left, _ := json.Marshal(expected)
-		right, _ := json.Marshal(got)
-		if bytes.Compare(left, right) != 0 {
-			t.Errorf("parse failed, expected %s, got %s", left, right)
-			return
-		}
+		check(t, expected, got)
 	}
 }
 
 func TestForm_NormalCase(t *testing.T) {
-	testcases := []*TestCase{
-		{
-			Title:     "normal case",
-			InputType: ProductQuery{},
-			InputForm: url.Values{
-				"created_at": {"1991-11-10T08:00:00+08:00"},
-				"color":      {"red"},
-				"is_soldout": {"true"},
-				"sort_by":    {"stock", "price"},
-				"sort_desc":  {"true", "true"},
-				"page":       {"1"},
-				"per_page":   {"20"},
-			},
-			Expected: &ProductQuery{
-				CreatedAt: time.Date(1991, 11, 10, 8, 0, 0, 0, time.FixedZone("+08:00", 8*3600)),
-				Color:     "red",
-				IsSoldout: true,
-				SortBy:    []string{"stock", "price"},
-				SortDesc:  []bool{true, true},
-				Pagination: Pagination{
-					Page:    1,
-					PerPage: 20,
-				},
+	parseFormAndCheck(
+		t,
+		url.Values{
+			"created_at": {"1991-11-10T08:00:00+08:00"},
+			"color":      {"red"},
+			"is_soldout": {"true"},
+			"sort_by":    {"stock", "price"},
+			"sort_desc":  {"true", "true"},
+			"page":       {"1"},
+			"per_page":   {"20"},
+		},
+		ProductQuery{},
+		&ProductQuery{
+			CreatedAt: time.Date(1991, 11, 10, 8, 0, 0, 0, time.FixedZone("+08:00", 8*3600)),
+			Color:     "red",
+			IsSoldout: true,
+			SortBy:    []string{"stock", "price"},
+			SortDesc:  []bool{true, true},
+			Pagination: Pagination{
+				Page:    1,
+				PerPage: 20,
 			},
 		},
-		{
-			Title:     "unsupported custom type",
-			InputType: &MessageQuery{},
-			InputForm: url.Values{
-				"uid":   {"ggicci"},
-				"after": {"5cb71995ad763f7f1717c9eb"},
-				"limit": {"50"},
-			},
-			ExpectedError: httpin.UnsupportedType("ObjectID"),
+	)
+}
+
+func TestForm_UnsupportedCustomType(t *testing.T) {
+	parseFormAndCheck(
+		t,
+		url.Values{
+			"uid":   {"ggicci"},
+			"after": {"5cb71995ad763f7f1717c9eb"},
+			"limit": {"50"},
 		},
-	}
-
-	for _, c := range testcases {
-		engine, err := httpin.NewEngine(c.InputType)
-		if err != nil {
-			t.Errorf("unable to create engine: %s", err)
-			t.FailNow()
-		}
-
-		got, err := engine.ReadForm(c.InputForm)
-		c.Check(t, got, err)
-	}
+		&MessageQuery{},
+		httpin.UnsupportedType("ObjectID"),
+	)
 }
